305569e6c34db1bb551ea93bdd0882b1a70477db
Leandro Temperoni <leandrotemperoni@gmail.com>
2023-02-11 21:37:55 -0300

    Change colors

--- app/src/main/java/uy/com/temperoni/recipes/ui/compose/navigation/Screen.kt
+++ app/src/main/java/uy/com/temperoni/recipes/ui/compose/navigation/Screen.kt
@@ -3,7 +3,7 @@ package uy.com.temperoni.recipes.ui.compose.navigation
 import uy.com.temperoni.recipes.R
 
 sealed class Screen(val route: String, val icon: Int) {
-    object Desserts : Screen("Postres", R.drawable.ic_baseline_restaurant_24)
+    object Desserts : Screen("Recetas", R.drawable.ic_baseline_restaurant_24)
     object Groceries : Screen("Compras", R.drawable.ic_baseline_local_grocery_store_24)
     object Chronometer : Screen("Cronometro", R.drawable.ic_baseline_timer_24)
 }
